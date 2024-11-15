b0381fb5b0015d7bb92d8edb3c806430e87f8482
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-15 13:19:51 +0200

    create BD

--- start_programm.py
+++ start_programm.py
@@ -1,9 +1,11 @@
 from GUI.password_window import PasswordWindow
 from PyQt6.QtWidgets import QApplication
+from Logics.database_work import connect_to_bd
 
 if __name__ == "__main__":
     app = QApplication([])
     
     start = PasswordWindow()
+    connect_to_bd()
     
     app.exec()
